d6c933ca8f67521f98a756286b12ef123a98c9a7
bydefaultcoder <bydefaultcoder@gmial.com>
2024-08-27 13:10:57 +0530

    df

--- myLibrary/settings.py
+++ myLibrary/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG') == 'True'
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
+# DEBUG = True
 
 
 import socket
