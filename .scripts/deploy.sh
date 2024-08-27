7dd632014273c9e47e6788354e1e8595f6296b4d
bydefaultcoder <bydefaultcoder@gmial.com>
2024-08-27 12:22:15 +0530

    ci-cd

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-set -e
 
-echo "Deployment started ..."
-
-# Pull the latest version of the app
-echo "Copying New changes...."
+# Pull the latest changes from the master branch
 git pull origin master
-echo "New changes copied to server !"
 
-# Activate Virtual Env
-#Syntax:- source virtual_env_name/bin/activate
-source mb/bin/activate
-echo "Virtual env 'mb' Activated !"
+# Activate the virtual environment
+source /home/applications/myLibrary/venv/bin/activate  # Adjust path to your virtual environment
 
-echo "Clearing Cache..."
-python manage.py clean_pyc
-python manage.py clear_cache
+# Install any new dependencies
+pip install -r requirements.txt
 
-echo "Installing Dependencies..."
-pip install -r requirements.txt --no-input
-
-echo "Serving Static Files..."
-python manage.py collectstatic --noinput
-
-echo "Running Database migration..."
-python manage.py makemigrations
+# Apply database migrations
 python manage.py migrate
 
-# Deactivate Virtual Env
-deactivate
-echo "Virtual env 'mb' Deactivated !"
+# Collect static files
+python manage.py collectstatic --noinput
 
-echo "Reloading App..."
-#kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
-ps aux |grep gunicorn |grep inner_project_folder_name | awk '{ print $2 }' |xargs kill -HUP
+# Restart the Django development server (stop it if it's already running)
+sudo pkill -f 'manage.py runserver'  # Kill existing server process if running
 
-echo "Deployment Finished !"
+# Start the Django development server in the background
+nohup python manage.py runserver 0.0.0.0:8000 > server.log 2>&1 &
