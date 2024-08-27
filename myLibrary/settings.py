d0decebae40c753d722172aeba4b457df3652642
bydefaultcoder <bydefaultcoder@gmial.com>
2024-08-27 19:37:53 +0530

    s

--- myLibrary/settings.py
+++ myLibrary/settings.py
@@ -125,7 +125,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [ TEMPLATES,],
-        'APP_DIRS': True,
+        'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
